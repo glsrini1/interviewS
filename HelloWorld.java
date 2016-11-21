b0c3e36dcf242dab471b267d03af7d7184bc8e8f
glsrini1 <glsdatacom@yahoo.com>
2016-11-21 11:33:43 -0500

    HelloWorld.java
    Sample Test file.

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// Hello World File
+public class HelloWorld  {
+  public staticvoid main(String... args) {
+          private final String name="GLS";
+          System.out.println("Hello "+ name);
+   }
+}
